d9d278925d9384a279048cd5b80a7dfec8c42bfa
MitkovskaM <mihaela.mitkovska@gmail.com>
2023-11-30 11:00:16 +0100

    Add first java proekt

---
+++ src/zadaca8/StepeniC.java
@@ -0,0 +1,18 @@
+package zadaca8;
+import java.util.Scanner;
+public class StepeniC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double c,f;
+Scanner tastatura=new Scanner(System.in);
+System.out.println("Vnesi stepeni celzusovi");
+c=tastatura.nextInt();
+f=9*c/5+32;
+System.out.println(c+"stepeni Celziusovi=");
+System.out.println(f+"Farenhajtovi stepeni");
+	}
+}
+		
+
+
